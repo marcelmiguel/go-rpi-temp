0eb22c36555d6e382f24e949d2153e916be1079e
Marcel <marcel.miguel@neogrup.com>
2015-08-18 17:12:55 +0200

    Project presentation

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-# RaspBerry Pi temperature with DS18B20 and 
+# RaspBerry Pi temperature with DS18B20
+
+## Introduction
+Test program in Go+, to check:
+ - cross compilling
+ - Web performance
+ - configuration 
+ - performance
+ - easy of development
+
+Based on: 
+https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/overview
+
+## WIP
 
 ## TODO
+ - read temp from RaspBerryPi
  - Configure system via web, 
-   . frquency update
-   . plotly configuration data
+   - frquency update
+   - plotly configuration data
  - store graph on Plot.ly
  - serve temperature via web, polymer
  - Run as a service
 
+## Hwd Schema
+Follow Adafruit link
+ 
+## Conclusions
+ - none yet
--- main.go
+++ main.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"os/exec"
+	"sync"
+	"strings"
 )
+
+func exe_cmd(cmd string, wg *sync.WaitGroup) {
+  fmt.Println("command is ",cmd)
+  // splitting head => g++ parts => rest of the command
+  parts := strings.Fields(cmd)
+  head := parts[0]
+  parts = parts[1:len(parts)]
+
+  out, err := exec.Command(head,parts...).Output()
+  if err != nil {
+    fmt.Printf("%s", err)
+  }
+  fmt.Printf("%s", out)
+  wg.Done() // Need to signal to waitgroup that this goroutine is done
+}
 
 func main() {
 	files, _ := ioutil.ReadDir("./")
@@ -14,4 +32,11 @@ func main() {
 	
 	files1, _ := filepath.Glob("*")
 	fmt.Println(files1)
+	
+	//testing execution of exe
+	wg := new(sync.WaitGroup)
+    wg.Add(1)
+    go exe_cmd("curl", wg)
+    wg.Wait()
+	
 }
