f3a4606b3a5abdfe23708d283c5cf77ecb3e42e7
marcelmiguel <marcel.miguel@gmail.com>
2015-08-21 15:25:38 +0200

    gitignore directory .diea

--- main.go
+++ main.go
@@ -1,152 +1,152 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-	"path/filepath"
-	"os"
-	"os/exec"
-	"sync"
-	"strings"
-	"strconv"
+	"encoding/json"
+	"fmt"
 	"go-rpi-temp/utils"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
-	"encoding/json"
 )
 
 type Temperature struct {
-    Celsius   float64
-    Farenheit float64
-	Id string //serial number
-	Valid bool
-	UpDown bool
+	Celsius   float64
+	Farenheit float64
+	Id        string //serial number
+	Valid     bool
+	UpDown    bool
 }
 
 var temp Temperature
 var prev Temperature
 
 func exe_cmd(cmd string, wg *sync.WaitGroup) {
-  fmt.Println("command is ",cmd)
-  // splitting head => g++ parts => rest of the command
-  parts := strings.Fields(cmd)
-  head := parts[0]
-  parts = parts[1:len(parts)]
+	fmt.Println("command is ", cmd)
+	// splitting head => g++ parts => rest of the command
+	parts := strings.Fields(cmd)
+	head := parts[0]
+	parts = parts[1:len(parts)]
 
-  out, err := exec.Command(head,parts...).Output()
-  if err != nil {
-    fmt.Print(err)
-  }
-  fmt.Println(out)
-  wg.Done() // Need to signal to waitgroup that this goroutine is done
+	out, err := exec.Command(head, parts...).Output()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println(out)
+	wg.Done() // Need to signal to waitgroup that this goroutine is done
 }
 
 func ReadTemp(file string) Temperature {
-  //fmt.Println("Filename ",file)
-  base := filepath.Base(filepath.Dir(file))
-  tmp := Temperature{}
-  tmp.Valid = false
-  tmp.Id = base[3:]
- 
-  inFile, err := os.Open(file)
-  if err != nil {
-     return tmp
-  }
+	//fmt.Println("Filename ",file)
+	base := filepath.Base(filepath.Dir(file))
+	tmp := Temperature{}
+	tmp.Valid = false
+	tmp.Id = base[3:]
 
-  defer inFile.Close()
-  scanner := bufio.NewScanner(inFile)
-  scanner.Split(bufio.ScanLines) 
-    
-  var lines []string
-
-  for scanner.Scan() {
-	lines = append(lines, scanner.Text())
-  }
-
-  for i:=0; i<len(lines); i++{
-	linea := lines[i]
-	
-	//fmt.Println(linea)
-	
-	if i==0 { //linea de estado
-	  //fmt.Println("Status ",linea[len(linea)-3:])
-	  if linea[len(linea)-3:] == "YES" {
-		tmp.Valid = true
-	  }
-	} else if i==1 { //linea de temperatura
-	  li := strings.LastIndexAny(linea,"t=")
-	  if li>=0 {
-	    celsius, err1 := strconv.ParseFloat(linea[li+1:], 64)
-		if err1 != nil {
-			tmp.Valid = false
-		} else {
-			tmp.Celsius = celsius / 1000.0
-			tmp.Farenheit = tmp.Celsius * 9.0 / 5.0 + 32.0 //TODO pasar a función del strcut			
-		}		
-	  }
+	inFile, err := os.Open(file)
+	if err != nil {
+		return tmp
 	}
-  }
 
-  return tmp
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for i := 0; i < len(lines); i++ {
+		linea := lines[i]
+
+		//fmt.Println(linea)
+
+		if i == 0 { //linea de estado
+			//fmt.Println("Status ",linea[len(linea)-3:])
+			if linea[len(linea)-3:] == "YES" {
+				tmp.Valid = true
+			}
+		} else if i == 1 { //linea de temperatura
+			li := strings.LastIndexAny(linea, "t=")
+			if li >= 0 {
+				celsius, err1 := strconv.ParseFloat(linea[li+1:], 64)
+				if err1 != nil {
+					tmp.Valid = false
+				} else {
+					tmp.Celsius = celsius / 1000.0
+					tmp.Farenheit = tmp.Celsius*9.0/5.0 + 32.0 //TODO pasar a función del strcut
+				}
+			}
+		}
+	}
+
+	return tmp
 }
 
 func handler(w http.ResponseWriter, r *http.Request, t <-chan Temperature) {
-	if len(t)>0 {
+	if len(t) > 0 {
 		prev = temp
-		temp = <- t
-		if prev.Celsius<temp.Celsius {
+		temp = <-t
+		if prev.Celsius < temp.Celsius {
 			temp.UpDown = true
 		} else {
 			temp.UpDown = false
 		}
 	}
 	if temp.Valid {
-		
-		fmt.Fprintf(w, "Temperature %s : %g", temp.Id, temp.Celsius)	
+
+		fmt.Fprintf(w, "Temperature %s : %g", temp.Id, temp.Celsius)
 	}
 }
 
 func handlerREST(w http.ResponseWriter, r *http.Request, t <-chan Temperature) {
-	if len(t)>0 {
+	if len(t) > 0 {
 		prev = temp
-		temp = <- t
-		if prev.Celsius<temp.Celsius {
+		temp = <-t
+		if prev.Celsius < temp.Celsius {
 			temp.UpDown = true
 		} else {
 			temp.UpDown = false
 		}
 	}
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-   	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(temp); err != nil {
-        fmt.Fprintf(w, "Error", err)
-    }
+		fmt.Fprintf(w, "Error", err)
+	}
 }
 func readData(configuration *conf.Configuration, t chan Temperature) {
-	files1, _ := filepath.Glob(configuration.Pathw1+"28*/w1_slave")
+	files1, _ := filepath.Glob(configuration.Pathw1 + "28*/w1_slave")
 
-    for {
-		if len(t)>0 {
-			<- t
+	for {
+		if len(t) > 0 {
+			<-t
 		}
-		for _, f:= range files1 {
+		for _, f := range files1 {
 			tmp := ReadTemp(f)
-			
+
 			t <- tmp
 			fmt.Println(tmp)
 		}
-		
+
 		time.Sleep(time.Duration(configuration.Frequency) * time.Second)
-    }
+	}
 }
 
 func main() {
 	//execute modprobes
 	wg := new(sync.WaitGroup)
-    wg.Add(2)
-    go exe_cmd("modprobe w1-gpio", wg)
+	wg.Add(2)
+	go exe_cmd("modprobe w1-gpio", wg)
 	go exe_cmd("modprobe w1-therm", wg)
-    wg.Wait()
+	wg.Wait()
 
 	configuration := conf.Open()
 	fmt.Println(configuration)
@@ -158,11 +158,11 @@ func main() {
 	fmt.Println("web server on localhost:8080")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-              handler(w, r, t)
-       })
+		handler(w, r, t)
+	})
 	http.HandleFunc("/api/v1/temp", func(w http.ResponseWriter, r *http.Request) {
-              handlerREST(w, r, t)
-       })
-    http.ListenAndServe(":8080", nil)
-	
+		handlerREST(w, r, t)
+	})
+	http.ListenAndServe(":8080", nil)
+
 }
--- utils/conf.go
+++ utils/conf.go
@@ -1,34 +1,34 @@
 package conf
 
 import (
-    "encoding/json"
-    "os"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 type Configuration struct {
-    PlotlyAPIToken  string
-    PlotlySecret   	string
-	Frequency       int	
-	Pathw1          string
+	PlotlyAPIToken string
+	PlotlySecret   string
+	Frequency      int
+	Pathw1         string
 }
 
-func Open() Configuration{
+func Open() Configuration {
 	configuration := Configuration{}
-	
+
 	file, err := os.Open("conf.json")
 	if err != nil {
-        return configuration
-    }
-	decoder := json.NewDecoder(file)	
+		return configuration
+	}
+	decoder := json.NewDecoder(file)
 	err1 := decoder.Decode(&configuration)
 	if err1 != nil {
-	  fmt.Println("error reading conf file:", err)
+		fmt.Println("error reading conf file:", err)
 	}
-	
+
 	if configuration.Frequency == 0 {
 		configuration.Frequency = 3
 	}
-	
+
 	return configuration
 }
